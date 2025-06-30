645a586e0e0f603ec09ed8d9fa975f5a05baa7f6
Alex Ma <chuchuma2000@gmail.com>
2025-06-30 19:55:33 -0400

    Parallax Backgrounds

---
+++ Assets/Scripts_pif/ParallaxEffect_pip.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class ParallaxEffect_pip : MonoBehaviour
+{
+    private float startPos;
+    private float length;
+    [SerializeField]
+    private Camera mainCam;
+    [SerializeField]
+    private float parallaxEffect;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = gameObject.transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distance = mainCam.transform.position.x * parallaxEffect;
+        float movement = mainCam.transform.position.x * (1 - parallaxEffect);
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (movement > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (movement < startPos - length)
+        {
+            startPos -= length;
+        }
+    }
+}
